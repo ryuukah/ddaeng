503a995f8df59e114e0c4d8a23a9d2df403fe666
ryuka <domodachiiwa@gmail.com>
2026-08-18 16:31:42 -0300

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const card = document.querySelector(".card__inner");
 
-card.addEventListener("click", () => {
+card.addEventListener("click", function () {
   card.classList.toggle("is-flipped");
 });
